f13b897335e776ee84fdfb5325221be96570473f
Tri Nguyen <tri@tridnguyen.com>
2014-02-16 00:13:57 -0500

    more explicit gh-pages for staging

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,6 +193,8 @@ module.exports = function(grunt) {
 			stage: {
 				options: {
 					base: '<%= config.buildPath %>',
+					branch: 'gh-pages',
+					repo: 'git@github.com:cbta/cbtallc.com.git'
 				},
 				src: ['**/*']
 			}
