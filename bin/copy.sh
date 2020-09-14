656b67009f5780692a7c6383e1e34aa366a32827
Tri Nguyen <tri@tridnguyen.com>
2020-09-13 21:23:00 -0400

    Add support for specifying copy destination

--- bin/copy.sh
+++ bin/copy.sh
@@ -11,6 +11,11 @@ set -f; IFS='
 '                           # turn off variable value expansion except for splitting at newlines
 for file in $ASSETS; do
 	set +f; unset IFS       # restore globbing and field splitting at all whitespace
-	cp -vf "$file" ./dist/
+	src=$(echo "$file" | cut -d' ' -f1 )
+	# -s to suppress cut output if " " delimter doesn't exist
+	# which means no dst declared
+	dst=$(echo "$file" | cut -s -d' ' -f2 )
+
+	cp -vf "$src" "./dist/$dst"
 done
 set +f; unset IFS           # do it again in case $ASSETS was empty
